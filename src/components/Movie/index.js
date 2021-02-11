571a97bfa7317e4ab6f0a685ff4151e4ab2b49b1
Semaias <semaiasrangel@gmail.com>
2021-02-10 23:44:58 -0300

    Ending favorites page

--- src/components/Movie/index.js
+++ src/components/Movie/index.js
@@ -1,7 +1,7 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-import {MdFavoriteBorder} from 'react-icons/md';
-import {FaSearch} from 'react-icons/fa';
+import { MdFavoriteBorder } from 'react-icons/md';
+import { FaSearch } from 'react-icons/fa';
 
 import './movie.css';
 
@@ -9,26 +9,56 @@ export default function Movie({
   id,
   title,
   year,
-}){
+}) {
   const [isFavorite, setIsFavorite] = useState(false);
 
   useEffect(() => {
-    if(localStorage.getItem(id)){
-      setIsFavorite(true);
+    if(localStorage.getItem("favoriteItem")){
+      const items = JSON.parse(localStorage.getItem("favoriteItem"));
+
+      items.forEach((item) => {
+        if(item.id === id){
+          setIsFavorite(true);
+        }
+      });
+
+
     }
   }, [localStorage]);
 
-  function handleFavorite(){
-    localStorage.setItem(id, {title, year, id});
-    setIsFavorite(true);
+  function handleFavorite() {
+    if(localStorage.getItem("favoriteItem")){
+      const items = JSON.parse(localStorage.getItem("favoriteItem"));
+
+      if (items) {
+        const safe = [...items, {title, year, id}]
+
+        localStorage.setItem("favoriteItem", JSON.stringify(safe));
+      }
+      setIsFavorite(true);
+    }else{
+      localStorage.setItem("favoriteItem", JSON.stringify([{ title, year, id }]));
+      setIsFavorite(true);
+    }
   }
 
-  function handleNotFavorite(){
-    localStorage.removeItem(id);
-    setIsFavorite(false);
+  function handleNotFavorite() {
+    if(localStorage.getItem("favoriteItem")){
+      let items =  JSON.parse(localStorage.getItem("favoriteItem"));
+
+      const item = items.findIndex(item => item.id === id);
+
+      console.log(item)
+
+      items.splice(item, 1);
+
+      localStorage.setItem("favoriteItem",  JSON.stringify(items));
+
+      setIsFavorite(false);
+    }
   }
 
-  return(
+  return (
     <div className="movie mt-3">
       <div className="infoMovie">
         <div>
@@ -38,13 +68,13 @@ export default function Movie({
       </div>
       {
         isFavorite ?
-        <button type="button">
-          <MdFavoriteBorder onClick={() => handleNotFavorite()} size={23} color="#000" />
-        </button>
-        :
-        <button type="button">
-          <MdFavoriteBorder onClick={() => handleFavorite()} size={23} color="#D4D4D4" />
-        </button>
+          <button type="button">
+            <MdFavoriteBorder onClick={() => handleNotFavorite()} size={23} color="#000" />
+          </button>
+          :
+          <button type="button">
+            <MdFavoriteBorder onClick={() => handleFavorite()} size={23} color="#D4D4D4" />
+          </button>
       }
     </div>
   )
--- src/pages/favorites/index.js
+++ src/pages/favorites/index.js
@@ -1,5 +1,73 @@
+import { useState, useEffect } from 'react';
+
+import {
+  Container,
+  Row,
+  Col
+} from 'react-bootstrap';
+
+import Nav from '../../components/Nav';
+import Movie from '../../components/Movie';
+
+import './styles.css';
+
 export default function Favorites(){
+  const [searchText, setSearchText] = useState('batman');
+  const [movies, setMovies] = useState([]);
+  const [itemNotFound, setItemNotFound] = useState(false);
+
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
+  function getFavorites(e){
+    if(localStorage.getItem('favoriteItem')) {
+      const items = JSON.parse(localStorage.getItem('favoriteItem'));
+      setMovies(items);
+    }else{
+      setItemNotFound(true);
+    }
+  }
+
+
   return(
-    <div>favorites</div>
-  )
+    <Container className="content">
+      <div className="w-100">
+        <Row>
+          <Col>
+            <div className="header">
+              <h2>CinemApp</h2>
+              <p>Bem-vindo ao mundo espetácular do cinema</p>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          {
+            itemNotFound ?
+            <>
+              <Col lg={12}>
+                Você não tem nada favoritado!
+              </Col>
+            </>
+            :
+            movies.map(item => (
+              <Col lg={6} sm={12}>
+                <Movie
+                  key={item.id}
+                  id={item.id}
+                  title={item.title}
+                  year={item.year}
+                />
+              </Col>
+            ))
+          }
+        </Row>
+      </div>
+      <Row>
+        <Col>
+          <Nav />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
